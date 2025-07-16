43b55090dcf6ada39fca8c8a5ee2c2a01aae1c7f
Pablo Javier <pablojavier@gmail.com>
2025-07-16 20:09:31 -0300

    Prepare release v0.2.4
    - Update version to 0.2.4
    - Update changelog
    - Ready for release

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,6 +1,6 @@
 class AppInfo {
   static const String name = 'LibreDrop';
-  static const String version = '0.2.3';
+  static const String version = '0.2.4';
   static const String description = 'Privacy-first cross-platform file sharing';
   static const String homepage = 'https://libredrop.org';
   static const String repository = 'https://github.com/pablojavier/libredrop';
